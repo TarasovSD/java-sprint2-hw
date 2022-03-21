1d47c4bfa9d4c1c33ca6fb6990f228ac086e373d
Sergey Tarasov <s.d.tarasov@ya.ru>
2022-03-21 22:01:42 +0300

    feat: добавить класс Manager

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        Manager manager = new Manager();
+
+        System.out.println("Список задач:");
+        System.out.println(manager.getTaskList());
+
+        System.out.println("Список подзадач:");
+        System.out.println(manager.getSubtaskList());
+
+        System.out.println("Список эпиков:");
+        System.out.println(manager.getEpicList());
+
+        manager.deleteAllTask();
+        System.out.println("Все задачи удалены!");
+
+        System.out.println("Задача с номером " + manager.taskNumber + ":");
+        manager.getTaskById();
+
+        manager.createTask();
+        System.out.println("Задача создана!");
+
+        manager.changeTask();
+        System.out.println("Задача изменена!");
+
+        manager.deleteTaskById();
+        ("Задача с номером " + manager.taskNumber + "удалена!");
+
+        manager.getSubtaskListEpic();
+        ("Список подзадач " + epic.name + " :");
+        manager.getSubtaskListEpic();
     }
 }
---
+++ src/Manager.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class Manager {
+    HashMap<Integer, Task> task =new HashMap<>();
+    HashMap<Integer, Subtask> subtask =new HashMap<>();
+    HashMap<Integer, Epic> epic =new HashMap<>();
+
+    private void putNewTasc() {
+
+    }
+
+    /* Получение списка всех задач типа Task */
+    private String getTaskList() {
+
+    }
+
+    /* Получение списка всех задач типа Subtask */
+    private String getSubtaskList() {
+
+    }
+
+    /* Получение списка всех задач типа Epic */
+    private String getEpicList() {
+
+    }
+}
